f70c35783f5587a1f1f2b6304c48459f88d41eed
mayukorin <19t7b2fx@gmail.com>
2020-08-13 18:11:56 +0900

    Add LoginFilter and Modify Person class

--- src/models/Person.java
+++ src/models/Person.java
@@ -8,8 +8,22 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
 
 
+@Table(name = "Persons")
+@NamedQueries({
+    @NamedQuery(
+            name ="checkRegisteredCode",
+            query = "SELECT COUNT(p) FROM Person AS p WHERE p.code = :code"
+            ),
+    @NamedQuery(
+            name = "checkLoginCodeAndPassword",
+            query = "SELECT p FROM Person AS p WHERE p.code = :code AND p.password = :pass")
+
+})
 
 @Entity
 @Embeddable
